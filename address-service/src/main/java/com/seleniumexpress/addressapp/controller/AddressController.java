e7c5565a39c27e5f7a1108b26288c7dc5a747d87
Aditya <adityasingh62065@gmail.com>
2023-12-27 20:13:02 +0530

    just added some comment

--- address-service/src/main/java/com/seleniumexpress/addressapp/controller/AddressController.java
+++ address-service/src/main/java/com/seleniumexpress/addressapp/controller/AddressController.java
@@ -2,7 +2,7 @@ package com.seleniumexpress.addressapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +15,8 @@ import com.seleniumexpress.addressapp.service.AddressService;
 public class AddressController {
 	@Autowired
 	private AddressService addressService;
+	
+	//Creating simple address controller
 	@GetMapping("/address/{employeeId}")
 	public ResponseEntity<AddressResponse> getAddressByEmployeeId(@PathVariable("employeeId") int id)
 	{
